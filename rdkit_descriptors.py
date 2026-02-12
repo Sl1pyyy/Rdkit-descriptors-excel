9ad5b805684b7fbb7587f3c6e02e7e2e6014149e
1447577 <prometey202020@gmail.com>
2026-02-12 13:14:32 +0100

    #2

--- rdkit_descriptors.py
+++ rdkit_descriptors.py
@@ -1,1 +1,20 @@
-import RDkit
+from pandas.core.interchange.dataframe_protocol import DataFrame
+from rdkit import Chem
+import requests
+import pandas as pd
+
+#Creation of an Excel file with SMILES annotations in the first column and name SMILES
+smiles_list = ['[H][C@]12SC(C)(C)[C@@H](N1C(=O)[C@H]2NC(=O)[C@H](N)C1=CC=C(O)C=C1)C(O)=O',
+'N[C@]([H])(C)C(=O)O',
+'N[C@@]([H])(C)C(=O)O'
+]
+
+df = pd.DataFrame(data =smiles_list, columns = ['SMILES'])
+df.to_excel('desc_table.xlsx', index = False)
+
+# def descriptor_structure():
+#
+#     return structure
+#
+# if __name__ == '__main__':
+#     structure = descriptor_structure()
